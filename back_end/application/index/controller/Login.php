51dd02df926bea1a5b6b6c2166082f4ecf914a33
DarkKris <long4664030@163.com>
2019-04-08 22:28:07 +0800

    init: init .gitignore file

---
+++ back_end/application/index/controller/Login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DarkKris
+ * Date: 2019/4/8
+ * Time: 下午9:19
+ */
---
+++ back_end/application/index/validate/UserValidate.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DarkKris
+ * Date: 2019/4/8
+ * Time: 下午9:20
+ */
+namespace app\index\validate;
+
+use think\Validate;
+
+class LoginValidate extends Validate {
+    protected $rule = [
+
+    ];
+
+    protected $message = [
+
+    ];
+
+    protected $scene = [
+        
+    ];
+}
