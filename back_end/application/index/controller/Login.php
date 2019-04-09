0295bbd0bc6d1ba1a71c3333d7514545af2b05b8
DarkKris <long4664030@163.com>
2019-04-09 11:47:32 +0800

    feat: back_end add some functions in user controller model

--- back_end/application/index/controller/Login.php
+++ back_end/application/index/controller/Login.php
@@ -13,6 +13,23 @@ use think\Controller;
 
 class Login extends Controller {
 
+    public function checkLogin() {
+        if(\Session::has('user_id')) {
+            $user_model = new UserModel();
+            $result = $user_model->isAdmin(session('user_id'));
+            if($result['code']==CODE_SUCCESS){
+                if($result['data']){
+                    return apiReturn(200,'ok','admin');
+                }else{
+                    return apiReturn(200,'ok','not-admin');
+                }
+            }else{
+                return apiReturn(500,$result['message'], $result['data']);
+            }
+        }
+        return apiReturn(200,'ok',false);
+    }
+
     public function login() {
         $user_validate = new UserValidate();
         $user_model = new UserModel();
@@ -24,9 +41,9 @@ class Login extends Controller {
             return apiReturn(500,$user_validate->getError(),[]);
 
         $result = $user_model->checkUser($req['username'],$req['password']);
-        if($result) {
-            session('user_id',$result['user_id']);
-            session('username',$result['username']);
+        if($result['code']==CODE_SUCCESS) {
+            session('user_id',$result['data']['user_id']);
+            session('username',$result['data']['username']);
             return apiReturn(200,'登录成功',null);
         }else{
             return apiReturn(403,'账号或密码输入错误，请重新输入。',null);
--- back_end/application/index/model/UserModel.php
+++ back_end/application/index/model/UserModel.php
@@ -22,11 +22,21 @@ class UserModel extends Model {
                 'password' => $hash_password
             ])->find();
             if($result) {
-                return $result;
+                return ['code'=>CODE_SUCCESS, 'message'=>'OK', 'data'=>$result];
             }else{
-                return null;
+                return ['code'=>CODE_SUCCESS, 'message'=>'OK', 'data'=>null];
             }
         }catch(Exception $e){
+            return ['code'=>CODE_ERROR, 'message'=>'数据库错误', 'data'=>$e->getMessage()];
+        }
+    }
+
+    public function isAdmin($user_id) {
+        try {
+            $info = $this->where('user_id')->find();
+            if ($info['admin'] == true) return ['code'=>CODE_SUCCESS, 'message'=>'OK', 'data'=>true];
+            else return ['code'=>CODE_SUCCESS, 'message'=>'OK', 'data'=>false];
+        }catch(Exception $e) {
             return ['code'=>CODE_ERROR, 'message'=>'数据库错误', 'data'=>$e->getMessage()];
         }
     }
